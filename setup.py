4615a04aa7503be0519e4852ad996af4eb564a54
Anurag Singh <singhanurag31july@gmail.com>
2024-12-27 21:50:39 +0530

    first commit

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name="mlproject",
+    version="0.0.1",
+    author="Anurag Singh",
+    author_email="singhanurag31july@gmail.com",
+    packages=find_packages(),
+    install_requires=[],
+)
